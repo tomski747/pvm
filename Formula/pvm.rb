76e6e2211528bff356808e9fae7c98aaca19c6ed
tomski747 <tomski747@users.noreply.github.com>
2024-12-09 19:49:17 +0200

    test

--- Formula/pvm.rb
+++ Formula/pvm.rb
@@ -1,7 +1,11 @@
 class Pvm < Formula
   desc "Pulumi Version Manager"
   homepage "https://github.com/tomski747/pvm"
-  head "https://github.com/tomski747/pvm.git", branch: "main"
+  url "https://github.com/tomski747/pvm.git", 
+      tag:      "v0.0.0",
+      revision: "homebrew"
+  head "https://github.com/tomski747/pvm.git", 
+      branch: "homebrew"
   
   depends_on "go" => :build
 
