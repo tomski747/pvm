68089e7dcfb3241a395b0296fdefd44219c548cb
tomski747 <tomski747@users.noreply.github.com>
2024-12-10 11:45:11 +0200

    Add --instal to 'pvm use'

--- internal/commands/use.go
+++ internal/commands/use.go
@@ -13,6 +13,7 @@ var useCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		version := args[0]
+		installIfMissing, _ := cmd.Flags().GetBool("install")
 
 		if version == "latest" {
 			latest, err := utils.GetLatestVersion()
@@ -27,6 +28,20 @@ var useCmd = &cobra.Command{
 			return fmt.Errorf("failed to resolve version: %w", err)
 		}
 
+		// Check if version is installed
+		installed := utils.GetInstalledVersions()
+		if !installed[resolvedVersion] {
+			if !installIfMissing {
+				return fmt.Errorf("version %s is not installed. Use 'pvm install %s' first or retry with --install flag", resolvedVersion, resolvedVersion)
+			}
+
+			// Install the version if --install flag is used
+			if err := utils.InstallVersion(resolvedVersion); err != nil {
+				return fmt.Errorf("failed to install version %s: %w", resolvedVersion, err)
+			}
+			fmt.Printf("%s %s\n", utils.Success("Successfully installed Pulumi"), resolvedVersion)
+		}
+
 		if err := utils.UseVersion(resolvedVersion); err != nil {
 			return fmt.Errorf("failed to switch to version %s: %w", resolvedVersion, err)
 		}
@@ -34,4 +49,8 @@ var useCmd = &cobra.Command{
 		fmt.Printf("%s %s\n", utils.Success("Switched to Pulumi"), resolvedVersion)
 		return nil
 	},
+}
+
+func init() {
+	useCmd.Flags().Bool("install", false, "Install the version if not already installed")
 }
